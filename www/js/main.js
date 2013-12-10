0ef899dcee9dcfe82abeef0c0c5b5adaccc25453
VolhaNazarenko <volha.nazarenko@gmail.com>
2013-12-10 18:52:08 +0300

    TonnaSkidok

---
+++ www/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() { 
+
+	if($('#banner1').length){
+		$("#banner1").carouFredSel({
+			items: 1,
+			auto : false, 
+			width: '100%',
+			pagination	: "#banner_pag"
+		});
+	}
+	if($('#partners').length){
+		$("#partners").carouFredSel({ 
+			auto : false, 
+			width: '100%',
+			prev: '#partners_prev',
+			next: '#partners_next',
+			pagination	: "#partners_pag"
+		});
+	}
+	
+});
